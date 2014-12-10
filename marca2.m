22ab26707da558f2b4c10c26144a4a1f3e3b848e
Martin Novy <martin1234@ufoni.cz>
2014-12-11 00:00:33 +0100

    fix meshgrid noscy

--- marca2.m
+++ marca2.m
@@ -1,10 +1,12 @@
-x11 = floor(linspace(xlo, xhi + 0.999, nosc));
+x11 = floor(linspace(xlo, xhi + 0.999, noscy));
+%x11 = floor(linspace(xlo, xhi + 0.999, nosc));
 %%bhi11 = roundb(linspace(bhilo, bhihi, nosc));
 %x11 = roundb(linspace(xlo, xhi, nosc));
 
+y11 = floor(linspace(yhi + 0.999 , ylo, nosc));
+%y11 = floor(linspace(yhi + 0.999 , ylo, noscy));
 %shi11 = roundb(linspace(shilo, shihi, noscy));
 %%shi11 = roundb(linspace(shihi, shilo, noscy));
-y11 = floor(linspace(yhi + 0.999 , ylo, noscy));
 
 %[bhi, shi] = meshgrid (bhi11, shi11);
 [xx, yy] = meshgrid (x11, y11);
--- marcar5.m
+++ marcar5.m
@@ -1,6 +1,9 @@
 %neigh=ones(3)  ; 
 %neigh=ones(5)  ;  
+neighold
 neigh = ones(11)
+%neigh = neighd6
+
 
 
 %xlo=2,  xhi=6  %   10
@@ -10,26 +13,36 @@ neigh = ones(11)
 %xlo=9,  xhi=10   %25 6
 xlo=40,  xhi=50  
 xlo=30,  xhi=60  
-xlo=-1,  xhi=122
 xlo=40,  xhi=45
+xlo=1,  xhi=80
+xlo=30,  xhi=70
+xlo=50,  xhi=65
+xlo=0,  xhi=122
+
 
 ylo=50, yhi=60
 ylo=40, yhi=70
-ylo=55, yhi=60
-%ylo=xlo, yhi=xhi   
+%ylo=50, yhi=65
+ylo=xlo, yhi=xhi   
 %ylo=xlo+1, yhi=xhi+1   
 
 bir= 34
-
 sur=bir
 
+%%%blo=  bir  ;
+%%% bhi=45  %    yy 
+%%% slo=sur;   %   
+ %%%shi=58     %   +1;
+ 
+
+
 sli= 1 /10    % 10  
-skipy=  2 %  1  1 7 % 19 
+skipy= 2  %  24    %  2  1  1 7 % 19 
 
 
 
-nosc=  768   %  512   %   256    384  %     1536   
-noscy =  nosc %   4 *64*2
+nosc=  730   %  512   %   256    384  %     1536   
+noscy = 800 % 2 *  nosc %   4 *64*2
 noscya = noscy / 2
 
 niter=2*2* 2*40 %2 
@@ -50,6 +63,6 @@ more off
 format bank
 %format + "o- ";
 
-
+%%%%marca26
 marca2
 
